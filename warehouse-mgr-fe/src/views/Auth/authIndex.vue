fc373afe0092f8e1771a539bb9583109cdf9ea14
EsperanceW <572934280@qq.com>
2022-04-05 21:21:33 +0800

    完成前端登录注册的相关界面

---
+++ warehouse-mgr-fe/src/views/Auth/authIndex.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="auth">
+    <div class="bg"></div>
+    <div class="title-info">
+      <img src="../../assets/img/warehouseIcon.png" alt="">
+      <h2 class="title">库存管理系统</h2>
+    </div>
+    <div class="form">
+      <a-tabs>
+        <a-tab-pane key="1" tab="登录">
+          <div class="item">
+            <a-input size="large" placeholder="账户">
+               <template #prefix>
+                 <UserOutlined />
+               </template>
+            </a-input>
+          </div>
+          <div class="item">
+          <a-input size="large" placeholder="密码">
+            <template #prefix>
+                 <LockOutlined />
+            </template>
+          </a-input>
+          </div>
+          <div class="item">
+            <a href="">忘记密码</a>
+          </div>
+          <div class="item">
+            <a-button size="large" type="primary">
+              登录
+            </a-button>
+          </div>
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="注册">
+          <div class="item">
+            <a-input size="large" placeholder="账户">
+              <template #prefix>
+                <UserOutlined />
+              </template>
+            </a-input>
+          </div>
+          <div class="item">
+          <a-input size="large" placeholder="密码">
+            <template #prefix>
+              <LockOutlined />
+            </template>
+          </a-input>
+          </div>
+          <div class="item">
+          <a-input size="large" placeholder="邀请码">
+            <template #prefix>
+              <MailOutlined />
+            </template>
+          </a-input>
+          </div>
+          <div class="item">
+            <a-button size="large" type="primary">
+              注册
+            </a-button>
+          </div>
+        </a-tab-pane>
+      </a-tabs>
+    </div>
+  </div>
+</template>
+
+<script src="./authIndex.js"></script>
+
+<style lang="scss" scoped>
+  @import './authIndex.scss';
+</style>
