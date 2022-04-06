c182e46914ed033ee1c037a9d7766aaa8b892bcc
EsperanceW <572934280@qq.com>
2022-04-06 15:48:54 +0800

    完成注册逻辑第一版

--- warehouse-mgr-be/src/index.js
+++ warehouse-mgr-be/src/index.js
@@ -1,32 +1,22 @@
 // 每个文件都是一个模块
-const koa = require('Koa');
+const Koa = require('koa');
+const koaBody = require('koa-body');
+const Body = require('koa-body');
+const { connect } = require('./db');
+const registerRoutes = require('./routers');
+const cors = require('@koa/cors')
 
-// const getYearByTimeStamp =  require('./helpers/utils/index');
+const app = new Koa();
 
-// console.log(getYearByTimeStamp(new Date().getTime()));
+connect().then(() => {
+    app.use(cors());
+    app.use(koaBody());
 
-const app = new koa();
+    registerRoutes(app);
 
-// 通过app.use注册中间件
-// 中间件本质上就是一个函数
-// 每次请求进来中间件都会被执行一次
-// context 上下文 - 当前请求的相关信息都在里面
-// app.use((context) => {
-//     // console.log(123);
-//     const { request: req } = context; // 对象的解构
-//     const { url } = req;
-
-//     if (url === '/user') {
-//         context.response.body = 'abcde';
-
-//         return;
-//     }
-
-//     context.body = '??';
-// });
-
-// 开启一个http服务
-// 接受http请求并做处理，处理完后响应
-app.listen(3000, () => {
-    console.log('启动成功');
+    // 开启一个http服务
+    // 接受http请求并做处理，处理完后响应
+    app.listen(3000, () => {
+        console.log('启动成功');
+    });
 });
--- warehouse-mgr-fe/src/main.js
+++ warehouse-mgr-fe/src/main.js
@@ -6,4 +6,8 @@ import store from './store';
 
 import 'ant-design-vue/dist/antd.css';
 
-createApp(App).use(store).use(router).use(Antd).mount('#app');
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(Antd)
+  .mount('#app');
